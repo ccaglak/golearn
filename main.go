f7717e9b32523de88ec9828d606265104cc010eb
ccaglak <ccaglak@gmail.com>
2024-12-01 17:07:45 +0300

    update

--- main.go
+++ main.go
@@ -1,15 +1,8 @@
 package main
 
-import (
-	"log"
-	"net/http"
-
-	"golearn/di"
-)
-
 func main() {
 	// di.Greet(os.Stdout, "Chad")
-	log.Fatal(
-		http.ListenAndServe("127.0.0.1:8000",
-			http.HandlerFunc(di.MyGreeterHandler)))
+	// log.Fatal(
+	//	http.ListenAndServe("127.0.0.1:8000",
+	//		http.HandlerFunc(di.MyGreeterHandler)))
 }
